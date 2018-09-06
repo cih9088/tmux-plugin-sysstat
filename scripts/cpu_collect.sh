22215dc0fcf69ef5344b0df2daef2526c7ba1088
Andy <ihcho@aitrics.com>
2018-09-06 18:12:33 +0900

    brew stdbuf

--- scripts/cpu_collect.sh
+++ scripts/cpu_collect.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 LC_NUMERIC=C
@@ -12,33 +13,39 @@ refresh_interval=$(get_tmux_option "status-interval" "5")
 samples_count="60"
 cpu_metric_file="$(get_tmux_option "@sysstat_cpu_tmp_dir" "/dev/null")/cpu_collect.metric"
 
+if command_exists "stdbuf"; then
+    stdbuf="stdbuf"
+else
+    stdbuf="gstdbuf"
+fi
+
 get_cpu_usage() {
   if is_osx; then
     if command_exists "iostat"; then
       iostat -w "$refresh_interval" -c "$samples_count" \
-        | stdbuf -o0 awk 'NR > 2 { print 100-$(NF-3); }'
+        | ${stdbuf} -o0 awk 'NR > 2 { print 100-$(NF-3); }'
     else
       top -l "$samples_count" -s "$refresh_interval" -n 0 \
         | sed -u -nr '/CPU usage/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)%[[:space:]]*idle.*/\1/p' \
-        | stdbuf -o0 awk '{ print 100-$0 }'
+        | ${stdbuf} -o0 awk '{ print 100-$0 }'
     fi
   elif ! command_exists "vmstat"; then
     if is_freebsd; then
       vmstat -n "$refresh_interval" -c "$samples_count" \
-        | stdbuf -o0 awk 'NR>2 {print 100-$(NF-0)}'
+        | ${stdbuf} -o0 awk 'NR>2 {print 100-$(NF-0)}'
     else
       vmstat -n "$refresh_interval" "$samples_count" \
-        | stdbuf -o0 awk 'NR>2 {print 100-$(NF-2)}'
+        | ${stdbuf} -o0 awk 'NR>2 {print 100-$(NF-2)}'
     fi
   else
     if is_freebsd; then
       top -d"$samples_count" \
         | sed -u -nr '/CPU:/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)%[[:space:]]*id.*/\1/p' \
-        | stdbuf -o0 awk '{ print 100-$0 }'
+        | ${stdbuf} -o0 awk '{ print 100-$0 }'
     else
       top -b -n "$samples_count" -d "$refresh_interval" \
         | sed -u -nr '/%Cpu/s/.*,[[:space:]]*([0-9]+[.,][0-9]*)[[:space:]]*id.*/\1/p' \
-        | stdbuf -o0 awk '{ print 100-$0 }'
+        | ${stdbuf} -o0 awk '{ print 100-$0 }'
     fi
   fi
 }
@@ -50,4 +57,3 @@ main() {
 }
 
 main
-
